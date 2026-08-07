e6457d3f51d587f8c125c4955a33b316cd71808b
Aditta Chakraborty <cseaditta@gmail.com>
2026-08-07 16:45:18 +0600

    send response util function implemented

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,38 +1,15 @@
 import { catchAsync } from "../../utils/catchAsync";
 import httpStatus from "http-status";
 import { NextFunction, Request, Response } from "express";
+import { sendResponse } from "../../utils/sendResponse";
 import { userService } from "./user.service";
-
-// const registerUser = async (req: Request, res: Response) => {
-//     try {
-//         const payload = req.body;
-
-//         const user = await userService.registerUserIntoDB(payload);
-
-//         res.status(httpStatus.CREATED).json({
-//             success: true,
-//             statusCode: httpStatus.CREATED,
-//             message: "User registered successfully",
-//             data: {
-//                 user
-//             }
-//         });
-//     } catch (error) {
-//         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-//             success: false,
-//             statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-//             message: "Failed to register user",
-//             error: (error as Error).message
-//         });
-//     }
-// }
 
 const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
     const user = await userService.registerUserIntoDB(payload);
 
-    res.status(httpStatus.CREATED).json({
+    sendResponse(res, {
         success: true,
         statusCode: httpStatus.CREATED,
         message: "User registered successfully",
