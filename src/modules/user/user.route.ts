ae3071eb5ffb90dd23519712f638620a445d751a
Aditta Chakraborty <cseaditta@gmail.com>
2026-08-10 02:34:09 +0600

    implemented jwt for access and refresh token

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -3,6 +3,7 @@ import { userController } from "./user.controller";
 
 const router = Router();
 
+router.get("/me", userController.getMyProfile);
 router.post("/register", userController.registerUser);
 
 export const userRoutes = router;
