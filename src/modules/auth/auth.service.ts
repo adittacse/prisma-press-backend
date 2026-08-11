4f41e8159f1ec4364c30ac0132cfae4cdcfee52e
Aditta Chakraborty <cseaditta@gmail.com>
2026-08-12 02:15:51 +0600

    refresh token api implemented

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
-import { SignOptions } from "jsonwebtoken";
+import { JwtPayload, SignOptions } from "jsonwebtoken";
 import config from "../../config";
 import { jwtUtils } from "../../utils/jwt";
 
@@ -50,6 +50,44 @@ const loginUserIntoDB = async (payload: ILoginUser) => {
     };
 }
 
+const refreshToken = async (refreshToken: string) => {
+    const verifiedRefreshToken = jwtUtils.verifyToken(refreshToken, config.JWT_REFRESH_SECRET);
+
+    if (!verifiedRefreshToken.success) {
+        throw new Error(verifiedRefreshToken.error);
+    }
+
+    const { id } = verifiedRefreshToken.data as JwtPayload;
+
+    const user = await prisma.user.findUniqueOrThrow({
+        where: {
+            id
+        }
+    });
+
+    if (user.activeStatus === "INACTIVE") {
+        throw new Error("User is Inactive!");
+    }
+
+    const jwtPayload = {
+        id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.JWT_ACCESS_SECRET,
+        config.JWT_ACCESS_EXPIRES_IN as SignOptions
+    );
+
+    return {
+        accessToken
+    }
+}
+
 export const authService = {
-    loginUserIntoDB
+    loginUserIntoDB,
+    refreshToken
 }
